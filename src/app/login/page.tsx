9276ee03f6891207e358d4b5ecf1c610c75ee396
Kamaluddin Arsyad Fadllillah <102853169+d-arsya@users.noreply.github.com>
2026-01-10 00:55:54 +0700

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Page() {
-  redirect(`${process.env.APP_URL}/login`); // redirect ke halaman /target
+  redirect(`/app/login`); // redirect ke halaman /target
   return null;
 }
